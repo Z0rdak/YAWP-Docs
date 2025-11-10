22a2634eafbab335a576741bc61705e7d8b21e70
Marcel Bartholet <Marcel.Bartholet@web.de>
2025-11-10 08:48:26 +0100

    fix deployment url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://z0rdak.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/yawp-Docs/',
+  baseUrl: '/yawp-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
